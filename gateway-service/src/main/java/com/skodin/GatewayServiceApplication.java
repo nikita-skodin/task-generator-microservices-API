ebc1f373bb5d07fb90224a41b73fbdd8b4a8c396
Nikita_Skodin <skodin36@gmail.com>
2023-11-25 15:03:48 +0300

    config commit

--- gateway-service/src/main/java/com/skodin/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/skodin/GatewayServiceApplication.java
@@ -1,7 +1,10 @@
 package com.skodin;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
